af8a20338f856b9b5ee4a40a1fcd986e82145367
janco <jancoweyers@gmail.com>
2021-10-07 21:47:05 +0200

    Fix add and remove miles

--- AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
+++ AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
@@ -6,21 +6,25 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import za.ac.nwu.ac.domain.persistence.AccountType;
-
 import java.time.LocalDate;
 
 @Repository
-public interface AccountTypeRepository extends JpaRepository<AccountType, Long> {
+public interface AccountTypeRepository extends JpaRepository<AccountType, Long>
+{
 
-    @Query(value = "SELECT " + "ACCOUNT_TYPE_ID," + "ACCOUNT_TYPE_NAME," + "CREATION_DATE," + "MNEMONIC" + "FROM " +
-            "VITRSA_SANDBOX.DEMO_ACCOUNT_TYPE " +"WHERE MNEMONIC = :mnemonic ", nativeQuery = true)
+    @Query(value = "SELECT " + "ACCOUNT_TYPE_ID," + "ACCOUNT_TYPE_NAME," + "CREATION_DATE," + "MNEMONIC" +
+                   "FROM " + "VITRSA_SANDBOX.DEMO_ACCOUNT_TYPE " +
+                   "WHERE MNEMONIC = :mnemonic ", nativeQuery = true)
     AccountType getAccountTypeByMnemonicNativeQuery(String mnemonic);
 
-    @Query(value = "SELECT " + "at " + "FROM " + "AccountType at " +"WHERE at.mnemonic = :mnemonic")
+    @Query(value = "SELECT " + "at " +
+                   "FROM " + "AccountType at " +
+                   "WHERE at.mnemonic = :mnemonic")
     AccountType getAccountTypeByMnemonic(String mnemonic);
 
-    @Query(value = "SELECT new za.ac.nwu.ac.domain.dto.AccountTypeDto(" +"at.mnemonic," + "at.accountTypeName," + "at.creationDate) " + "FROM " +
-            "AccountType at " + "WHERE at.mnemonic = :mnemonic")
+    @Query(value = "SELECT new za.ac.nwu.ac.domain.dto.AccountTypeDto(" +"at.mnemonic," + "at.accountTypeName," + "at.creationDate) " +
+                   "FROM " + "AccountType at " +
+                   "WHERE at.mnemonic = :mnemonic")
     AccountType getAccountTypeDtoByMnemonic(String mnemonic);
 
 
